003fd530784ef2685c1f85f9b391afa783b971f7
Cerulity64X <cerulity32k@gmail.com>
2023-05-12 23:49:56 -0400

    Add very simple README

---
+++ README.md
@@ -0,0 +1,2 @@
+# Defancifier
+An unfathomably simple one-liner that wraps my `defancify` library for standard I/O. Pass text in through program arguments, and the result will be printed out.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,1 @@
-fn main() {
-    println!( "{}", defancify::allocs::defancify_lossy(
-        &std::env::args().skip(1).next().unwrap_or("No arguments passed in!".to_string()).to_owned(),
-        None
-    ))
-}
+fn main() { println!("{}", defancify::allocs::defancify_lossy(&std::env::args().skip(1).next().unwrap_or("No arguments passed in!".to_string()).to_owned(), None))}
